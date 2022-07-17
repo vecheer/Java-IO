70aeec887f9103d36e1e68c43507a2287cabaf53
vecheer <1571462409@.com>
2022-07-17 11:42:18 +0800

    nio 服务端

--- CH2-NIO/src/main/java/L3_selector/C1_nio_client.java
+++ CH2-NIO/src/main/java/L3_selector/C1_nio_client.java
@@ -1,6 +1,5 @@
 package L3_selector;
 
-import com.google.common.base.Charsets;
 import lombok.extern.log4j.Log4j;
 import utils.time.Timer;
 
@@ -8,7 +7,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 @Log4j
 public class C1_nio_client {
@@ -17,8 +15,10 @@ public class C1_nio_client {
             SocketChannel channel = SocketChannel.open();
 
             channel.connect(new InetSocketAddress("localhost", 8080));
-            channel.write(Charsets.UTF_8.encode("wyq"));
-            Timer.sleep(3000);
+            //channel.write(Charset.defaultCharset().encode("我准备发信息了\n这是wuyuqi吴宇奇发的消息"));
+            channel.write(Charset.defaultCharset().encode("abc4abc8abc12abc16abc\n这是wuyuqi吴宇奇发的消息\n")); // 结尾得加一个\n不然服务端以为是半包
+            //channel.write(Charset.defaultCharset().encode("abc\n1233\n"));
+            Timer.sleepForever();
             channel.close();
         } catch (IOException e) {
             e.printStackTrace();
--- CH2-NIO/src/main/java/L3_selector/C1_selector.java
+++ CH2-NIO/src/main/java/L3_selector/C1_selector.java
@@ -10,11 +10,16 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 @Log4j
 public class C1_selector {
+
+
     public static void main(String[] args) throws IOException {
 
 
@@ -40,8 +45,8 @@ public class C1_selector {
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 iterator.remove();
-                System.out.println(key.channel());
-                System.out.println(key.interestOps());
+                log.info("本次接收到的channel是: " + key.channel());
+                log.info("本channel感兴趣的事件是: " + key.interestOps());
                 // accept 事件
                 if (key.isAcceptable()) {
                     log.info("key isAcceptable");
@@ -50,7 +55,9 @@ public class C1_selector {
                     // do after accepted the socket channel
                     channel.configureBlocking(false);
                     // register the channel received
-                    SelectionKey channelKey = channel.register(selector, 0, null);
+                    // 为该channel指定1个附件，每个channel都有自己的附件，防止多个channel公用一个buffer引发的数据混乱
+                    ByteBuffer buffer = ByteBuffer.allocate(5);
+                    SelectionKey channelKey = channel.register(selector, 0, buffer);
                     channelKey.interestOps(SelectionKey.OP_READ);
                 }
                 // read 事件
@@ -58,13 +65,26 @@ public class C1_selector {
                     log.info("key isReadable");
                     try{
                         SocketChannel channel = (SocketChannel) key.channel();
-                        ByteBuffer buffer = ByteBuffer.allocate(5);
+                        ByteBuffer buffer = (ByteBuffer) key.attachment(); // 取出该channel的buffer
                         int read = channel.read(buffer);
+                        log.info("当前从channel读取到的字节数为: " + read);
                         if (read == -1){
                             key.cancel();
                         } else {
                             // handle buffer
-                            System.out.println(Charsets.UTF_8.decode(buffer));
+                            List<ByteBuffer> bufferList = bufferParse(buffer, '\n');
+                            log.info("本次buffer被拆分为" + bufferList.size() + "个list");
+                            // 读取完buffer的compact操作之后，如果position和limit相等，则说明整个包都没有遇到换行符
+                            // 进一步可以判断出是包太大，buffer放不下
+                            // 这种情况下就需要对buffer进行扩容
+                            if (buffer.position() == buffer.limit()){
+                                ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity() * 2);
+                                buffer.flip();
+                                newBuffer.put(buffer);
+                                key.attach(newBuffer); //使用扩容后的buffer，替换老buffer
+                            }
+
+                            bufferList.forEach(one-> log.info("本次读取的内容是: " + StandardCharsets.UTF_8.decode(one)));
                         }
                     }catch (IOException e) {
                         log.error(e);
@@ -76,4 +96,27 @@ public class C1_selector {
             Timer.sleep(2000);
         }
     }
+
+    // 解析收到的buffer，并按照粘包和拆包解析
+    public static List<ByteBuffer> bufferParse(ByteBuffer source, char splitChar){
+        final List<ByteBuffer> receivedCache = new LinkedList<>();
+        // 置为读
+        source.flip();
+        int mark = 0;
+        for (int i = 0; i < source.limit(); i++) {
+            if ((char)source.get(i) == splitChar) {
+                ByteBuffer temp = ByteBuffer.allocate(i-mark+1);
+                for (int j = mark; j <= i; j++) {
+                    temp.put(source.get());
+                }
+                mark = i+1;
+                temp.flip();
+                receivedCache.add(temp);
+            }
+        }
+        // 切回写，没读的部分不动，留到下一次继续处理
+        source.compact();
+        return receivedCache;
+    }
+
 }
