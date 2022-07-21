d72fba056584bc21bbac11c14d8847c3f61a3b63
vecheer <1571462409@qq.com>
2022-07-21 10:26:05 +0800

    专用于netty客户端测试

---
+++ CH3-Netty/src/main/java/Netty_test_Client.java
@@ -0,0 +1,27 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class Netty_test_Client {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    // 在连接建立后会调用
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost",8080))
+                .sync()
+                .channel()
+                .writeAndFlush("hello!");
+
+    }
+}
