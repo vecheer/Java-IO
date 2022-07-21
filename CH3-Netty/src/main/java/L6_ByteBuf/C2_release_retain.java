a3d4e3bf7f9a36c82a8877eef3644084d211fab6
vecheer <1571462409@.com>
2022-07-22 00:02:09 +0800

    netty中应该在最后处理buf的handler中，将该bufrelease，减少该buf的使用计数。这样底层就会对该buf进行回收。

---
+++ CH3-Netty/src/main/java/L6_ByteBuf/C2_release_retain.java
@@ -0,0 +1,92 @@
+package L6_ByteBuf;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+import utils.concurrent.atomic.YqAtomicInt;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author yq
+ * @version 1.0
+ * @date 2022/7/21 23:42
+ */
+@Log4j
+public class C2_release_retain {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        // 往pipeline中添加入站handler
+                        // 1.name 处理（由客户端传入）
+                        pipeline.addLast("in-1-name_assembly", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("in-1-name_assembly processing");
+                                String name = ((ByteBuf) msg).toString(StandardCharsets.UTF_8);
+                                ((ByteBuf) msg).release();
+                                Student student = new Student(null, name);
+                                super.channelRead(ctx, student);
+                            }
+                        });
+                        // 2.ID 处理（由原子类自增）
+                        pipeline.addLast("in-2-setting_Id", new ChannelInboundHandlerAdapter() {
+                            private final YqAtomicInt count =  new YqAtomicInt(0);
+
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("in-2-setting_Id processing");
+                                Student student = (Student) msg;
+                                student.setId(count.getAndIncrement());
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        // 3.打印信息
+                        pipeline.addLast("in-3-print_info", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("in-3-print_info processing");
+                                Student student = (Student) msg;
+                                log.info("得到的结果是: " + msg);
+                                ch.writeAndFlush(msg);
+                            }
+                        });
+                        // 往pipeline中添加出站handler
+                        pipeline.addLast("out-1",new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("out-1");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("out-2",new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("out-2");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class Student{
+        private Integer id;
+        private String name;
+    }
+}
